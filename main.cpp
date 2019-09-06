f1531f7c94c28ded7a93f85f4d4dcea0c3b38b35
Paulos Tegegn <pstegegn@gmail.com>
2019-09-05 19:58:34 -0500

    thread pool manager created

--- main.cpp
+++ main.cpp
@@ -1,8 +1,32 @@
 #include <iostream>
 #include "PigLatinConverter.h"
+#include "ThreadPool.h"
+#include <thread>
 
 int main() {
     PigLatinConverter pigLatinConverter;
-    std::cout << "Hello, World!" << std::endl;
+    std::vector<std::future<int> > results;
+
+
+    //create thread pool manager
+    ThreadPool threadPoolMgr(std::thread::hardware_concurrency() - 2);
+
+
+    while (true) {
+        std::string text;
+        std::cout<<"Enter Text:";
+        getline(std::cin,text);
+        if(text == "q" || text == "quit"|| text=="exit") {
+            break;
+        }
+        results.emplace_back(
+                threadPoolMgr.enqueue([] {
+                    std::cout << "hello from a thread" << std::endl;
+                    std::this_thread::sleep_for(std::chrono::seconds(1));
+                    return 1;
+                })
+        );
+    }
+
     return 0;
 }
